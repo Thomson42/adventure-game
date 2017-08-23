e12d91bbbf725b386655c413cf84e9bf607aadff
Christopher Thomson <christopher.thomson42@gmail.com>
2017-08-23 09:38:11 -0700

    removed unesssary file and made better challanges

--- src/modules/challenges.js
+++ src/modules/challenges.js
@@ -8,23 +8,59 @@ const challenges =  [
     correctAnswer: 'A'
   },
   {
-    question: 'What is 2 + 2 num2?',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
+    question: 'What are you doing here!?',
+    answerA: 'I am going to class',
+    answerB: 'step off man',
+    answerC: 'eat my shorts',
+    answerD: 'whatever',
     correctAnswer: 'A'
   },
   {
-    question: 'What is 2 + 2 num3?',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
-    correctAnswer: 'A',
+    question: 'what dose super() do in a constructor?',
+    answerA: 'Makes you super powerful',
+    answerB: 'Allows you to use the this keyword inside the constructor',
+    answerC: 'Lets you use props',
+    answerD: 'Makes the class special',
+    correctAnswer: 'B',
     state: 'incorrect'
   },  {
-    question: 'What is 2 + 2? num4',
+    question: 'What are you doing with all that stuff?',
+    answerA: 'notin',
+    answerB: 'What are you talking about?',
+    answerC: 'I have a science project sir that is due tomorrow',
+    answerD: 'whatever',
+    correctAnswer: 'C',
+    state: 'incorrect'
+  },
+  {
+    question: 'What makes Snap shot testing different from TDD',
+    answerA: 'Tests are for suckers',
+    answerB: 'They aren\'t that different',
+    answerC: 'Stuff and things',
+    answerD: 'It is indented to make UI easer to verify not to guarantee UI development though testing',
+    correctAnswer: 'D',
+    state: 'incorrect'
+  },
+  {
+    question: 'How is it that there are 10 different challenges?',
+    answerA: 'too much time on our hands I guess',
+    answerB: 'what?',
+    answerC: 'this is just a filler question isn\'t it?',
+    answerD: '8',
+    correctAnswer: 'A',
+    state: 'incorrect'
+  },
+  {
+    question: 'What do you use propTypes for?',
+    answerA: 'to verify that objects don\'t change their typing from when the component was made',
+    answerB: 'I ain\'t no coder',
+    answerC: 'to set a type of prop',
+    answerD: 'your guess is as good as mine',
+    correctAnswer: 'A',
+    state: 'incorrect'
+  },
+  {
+    question: 'What is 2 + 2?',
     answerA: '4',
     answerB: '5',
     answerC: '6',
@@ -33,56 +69,20 @@ const challenges =  [
     state: 'incorrect'
   },
   {
-    question: 'What is 2 + 2? num5',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
+    question: 'Why is 2 + 2? ',
+    answerA: 'uhh...4?',
+    answerB: 'why is anything',
+    answerC: 'is this a question or a typo?',
+    answerD: 'fish',
     correctAnswer: 'A',
     state: 'incorrect'
   },
   {
-    question: 'What is 2 + 2? num6',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
-    correctAnswer: 'A',
-    state: 'incorrect'
-  },
-  {
-    question: 'What is 2 + 2? num7',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
-    correctAnswer: 'A',
-    state: 'incorrect'
-  },
-  {
-    question: 'What is 2 + 2? num8',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
-    correctAnswer: 'A',
-    state: 'incorrect'
-  },
-  {
-    question: 'What is 2 + 2? num9',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
-    correctAnswer: 'A',
-    state: 'incorrect'
-  },
-  {
-    question: 'What is 2 + 2? num10',
-    answerA: '4',
-    answerB: '5',
-    answerC: '6',
-    answerD: '8',
+    question: 'This is the last straw Percy Prankster your mine now!(He\'s comping right at you!)',
+    answerA: 'doge left',
+    answerB: 'doge right',
+    answerC: 'jump over him',
+    answerD: 'barrel roll',
     correctAnswer: 'A',
     state: 'incorrect'
   }
